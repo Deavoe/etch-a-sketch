4a8d576ec0f06ad4e1e6b0bdfb2b4616eab3fc06
Deavoe <123446640+Deavoe@users.noreply.github.com>
2023-02-02 15:26:06 -0500

    Add final touches and add live preview to README.md

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 A project that tests my fundamentals of JavaScript - based off the classic etch-a-sketch game.
 Apart of The Odin Project's Curriculum.
 
-Live Preview:
+Live Preview: https://deavoe.github.io/etch-a-sketch/
--- script.js
+++ script.js
@@ -1,27 +1,12 @@
-function shadeColor(color, percent) {
+// function shadeColor(col, amt) {
+//     var num = parseInt(color,16),
+//     amt = Math.round(2.55 * percent),
+//     R = (num >> 16) + amt,
+//     B = (num >> 8 & 0x00FF) + amt,
+//     G = (num & 0x0000FF) + amt;
 
-    var R = parseInt(color.substring(1,3),16);
-    var G = parseInt(color.substring(3,5),16);
-    var B = parseInt(color.substring(5,7),16);
-
-    R = parseInt(R * (100 + percent) / 100);
-    G = parseInt(G * (100 + percent) / 100);
-    B = parseInt(B * (100 + percent) / 100);
-
-    R = (R<255)?R:255;  
-    G = (G<255)?G:255;  
-    B = (B<255)?B:255;  
-
-    R = Math.round(R)
-    G = Math.round(G)
-    B = Math.round(B)
-
-    var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
-    var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
-    var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
-
-    return "#"+RR+GG+BB;
-} // Darken 10% : shadeColor("#FFFFFF",-10);
+//     return (0x1000000 + (R<255?R<1?0:R:255)*0x10000 + (B<255?B<1?0:B:255)*0x100 + (G<255?G<1?0:G:255)).toString(16).slice(1);
+// }
 
 // Chronological Checklist
 // [X] Base webpage
@@ -40,19 +25,35 @@ slider.oninput = function() {
     output.textContent = this.value;
   }
 
+function rgbToHex(r, g, b) {
+return "#" + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1);
+}
 function hoverColor(e) {
-    if (rgb === false) {
+    if (rgb === false && e.target.style.backgroundColor === "rgb(255, 255, 255)"  ) {
         e.target.style.backgroundColor = "rgb(0,0,0)";
-    } else if (e.target.style.backgroundColor === "rgb(255, 255, 255)" ) {
-        e.target.style.backgroundColor = '#' + parseInt(Math.random() * 0xffffff).toString(16)
-    } else {
-        shadeColor(e.target.style.backgroundColor, -10 );
+    } else if (e.target.style.backgroundColor === "rgb(255, 255, 255)" ) { //(e.target.className === "grd" ) { //
+        let rc = '#' + parseInt(Math.random() * 0xffffff).toString(16)
+        e.target.style.backgroundColor = rc
+        e.target.className = '#' + parseInt(Math.random() * 0xffffff).toString(16)
+    // } else {
+    //     e.target.style.backgroundColor = shadeColor(e.target.className, -10) ;
     }
 }
 
-function genGrid() {
-let num = slider.value
+function removeTransition(e) {
+    // if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('hide');
+  }
+
 let container = document.querySelector("#container")
+container.addEventListener('transitionend', removeTransition);
+
+function genGrid(bool) {
+let num = slider.value
+// let container = document.querySelector("#container")
+if (bool !== false ) {
+    container.classList.toggle("hide")
+}
 container.replaceChildren(); // clears children
 // grid-template-columns: repeat(16,16fr);
 container.style.setProperty('grid-template-columns', 'repeat('+num+','+num+'fr)');
@@ -63,6 +64,7 @@ elem.style.outlineStyle = "solid";
 elem.style.outlineColor = "rgb(200,200,200)"
 elem.style.outlineWidth = "1px";
 elem.style.overflow = "hidden";
+elem.style.transition = "all 0.12s";
 elem.style.backgroundColor = "rgb(255,255,255)";
 elem.style.height = "auto";
 elem.classList.add("grd");
@@ -74,6 +76,7 @@ console.log("Generated grid.")
 document.querySelector(".genb").addEventListener("click",genGrid);
 
 function resetGrid() {
+    container.classList.add("hide")
     let nodes = document.getElementById('container').childNodes;
     for(var i=0; i<nodes.length; i++) {
     if (nodes[i].nodeName.toLowerCase() == 'div') {
@@ -89,3 +92,9 @@ function toggleRGB() {
     console.log("Toggled RGB. " + rgb) 
 }
 document.querySelector(".toggle").addEventListener("click",toggleRGB);
+
+document.querySelector('.title').classList.remove("hover")
+
+genGrid(false)
+
+document.querySelector('body').classList.remove("hover2")
